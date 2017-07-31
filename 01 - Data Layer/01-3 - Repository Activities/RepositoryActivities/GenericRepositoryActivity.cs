2857af8d3ef724301d1eafa089843ba1d45f361b
Sasa Malezic <sasa.malezic@bluecouchdigital.com>
2017-07-31 23:20:55 +0200

    Added code in GenericRepositoryActivity class.

--- 01 - Data Layer/01-3 - Repository Activities/RepositoryActivities/GenericRepositoryActivity.cs
+++ 01 - Data Layer/01-3 - Repository Activities/RepositoryActivities/GenericRepositoryActivity.cs
@@ -1,12 +1,168 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using ADS.BankingAnalytics.DataEntities.DataBroker;
 
 namespace ADS.BankingAnalytics.DataEntities.RepositoryActivities
 {
     public class GenericRepositoryActivity
     {
+        #region Fields
+
+        //private static readonly ILog _logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private OrganizationalStructureDbContext _context;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public GenericRepositoryActivity()
+        {
+            _context = new OrganizationalStructureDbContext();
+        }
+
+        #endregion Constructors
+
+        #region Interface Implementation
+
+        public T GetById<T>(object[] pk, params Expression<Func<T, object>>[] includeExpressions) where T : class
+        {
+            return _context.Set<T>().Find(pk);
+        }
+
+        public IQueryable<T> GetAll<T>(params Expression<Func<T, object>>[] includeExpressions) where T : class
+        {
+            var retVal = _context.Set<T>();
+
+            if (includeExpressions.Any())
+            {
+                var set = includeExpressions
+                  .Aggregate<Expression<Func<T, object>>, IQueryable<T>>
+                    (retVal, (current, expression) => current.Include(expression));
+            }
+
+            return retVal;
+        }
+
+        public IEnumerable<T> GetByCriteria<T>(
+            Expression<Func<T, bool>> criteria,
+            params Expression<Func<T, object>>[] includeStatements
+            ) where T : class
+        {
+            IQueryable<T> retVal = _context.Set<T>();
+
+            if (criteria != null)
+            {
+                retVal = retVal.Where(criteria);
+            }
+
+            foreach (var includeStatement in includeStatements)
+            {
+                retVal = retVal.Include(includeStatement);
+            }
+
+            return retVal;
+        }
+
+        public IEnumerable<T> GetJoinnedWithCriteria<T, K>(Func<T, bool> criteria) where T : class where K : class
+        {
+            throw new NotImplementedException();
+
+            //return context.Sellings
+            //        .Join(context.Actions,
+            //            selling => selling.Id,
+            //            action => action.Id,
+            //            (selling, action) => new { Selling = selling, Action = action }
+            //        );
+        }
+
+        public T Save<T>(T entity, params object[] pk) where T : class
+        {
+            var entry = _context.Entry<T>(entity);
+
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    // Leave it as it is
+                    break;
+
+                case EntityState.Deleted:
+                    // It's not to be deleted after all
+                    entry.State = EntityState.Modified;
+                    break;
+
+                case EntityState.Detached:
+                    // Let's see if it really exists in Db
+                    T foundEntity = null;
+                    if (pk != null && pk.Length > 0)
+                    {
+                        // Ako je pk tipa int proveriti da li je razlicito od 0, ukoliko nije int ulazi u blok
+                        if (pk.Any(it => it != null && (it.GetType() == typeof(int) && ((int)it) > 0) || (it.GetType() != typeof(int))))
+                        {
+                            foundEntity = _context.Set<T>().Find(pk);
+                        }
+                    }
+                    if (foundEntity == null)
+                    {
+                        entry.State = EntityState.Added;
+                    }
+                    else
+                    {
+                        entry = _context.Entry(foundEntity);
+                        entry.CurrentValues.SetValues(entity);
+                        entry.State = EntityState.Modified;
+                    }
+                    break;
+
+                case EntityState.Modified:
+                    // Leave it as it is
+                    break;
+
+                case EntityState.Unchanged:
+                    // Leave it as it is
+                    break;
+
+                default:
+                    break;
+            }
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            //catch (System.Data.Entity.Validation.DbEntityValidationException enValEx)
+            catch (Microsoft.EntityFrameworkCore.DbUpdateException enValEx)
+            {
+                //TODO: Do proper logging of DbContext exception!
+
+                //_logger.FatalFormat("Saving an entry of type '{0}' has failed because of entity validation.", typeof(T).FullName);
+                //_logger.FatalFormat("Error (StackTrace) - {0}", enValEx.StackTrace);
+                //_logger.FatalFormat("Error (InnerException) - {0}", enValEx.InnerException);
+                //_logger.FatalFormat("Error (Message) - {0}", enValEx.Message);
+
+                throw enValEx;
+            }
+            catch (Exception exc) // for debugging
+            {
+                //_logger.FatalFormat("Error (StackTrace) - {0}", exc.StackTrace);
+                //_logger.FatalFormat("Error (InnerException) - {0}", exc.InnerException);
+                //_logger.FatalFormat("Error (Message) - {0}", exc.Message);
+
+                throw exc;
+            }
+
+            return entry.Entity;
+        }
+
+        #endregion Interface Implementation
+
+        #region Private Methods
+
+
+
+        #endregion Private Methods
     }
 }
---
+++ 01 - Data Layer/01-3 - Repository Activities/RepositoryActivities/IGenericRepositoryActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ADS.BankingAnalytics.DataEntities.RepositoryActivities
+{
+    public interface IGenericRepositoryActivity
+    {
+        #region Fetch
+
+        T GetById<T>(object[] pk, params Expression<Func<T, object>>[] includeExpressions) where T : class;
+        IQueryable<T> GetAll<T>(params Expression<Func<T, object>>[] includeExpressions) where T : class;
+        IEnumerable<T> GetByCriteria<T>(
+            Expression<Func<T, bool>> criteria,
+            params Expression<Func<T, object>>[] includeStatements
+            ) where T : class;
+        IEnumerable<T> GetJoinnedWithCriteria<T, K>(Func<T, bool> criteria) where T : class where K : class;
+
+        #endregion Fetch
+
+        #region Insert & Update
+
+        T Save<T>(T entity, params object[] pk) where T : class;
+
+        #endregion Insert & Update
+    }
+}
