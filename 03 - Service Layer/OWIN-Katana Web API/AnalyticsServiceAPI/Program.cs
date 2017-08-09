22da8250624d89ca4094e4b84e6d31c1261a3c94
Sasa Malezic <sasa.malezic@bluecouchdigital.com>
2017-08-09 07:12:31 +0200

    New files added.

---
+++ 03 - Service Layer/OWIN-Katana Web API/AnalyticsServiceAPI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+
+namespace ADS.BankingAnalytics.AnalyticsServiceAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebApp.Start<Startup>("http://localhost:8086");
+
+            Console.WriteLine("Server is started. Press Enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03 - Service Layer/OWIN-Katana Web API/AnalyticsServiceAPI/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Owin;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADS.BankingAnalytics.AnalyticsServiceAPI
+{
+    using AppFunc = Func<IDictionary<String, Object>, Task>;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var middleware = new Func<AppFunc, AppFunc>(MyMiddleware);
+            app.Use(middleware);
+        }
+
+        public AppFunc MyMiddleware(AppFunc next)
+        {
+            AppFunc appFunc = async (IDictionary<String, Object> environment) =>
+            {
+                var response = environment["owin.ResponseBody"] as Stream;
+
+                using (var writer = new StreamWriter(response))
+                {
+                    await writer.WriteAsync("<h1>Hello from my first middleware</h1>");
+                }
+
+                await next.Invoke(environment);
+            };
+
+            return appFunc;
+        }
+    }
+}
